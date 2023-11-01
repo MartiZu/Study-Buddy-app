2df4ecf478821caca7cf2a97b8c0214a57e71d09
MartiZu <martina.zurli@gmail.com>
2023-11-01 19:39:25 +0000

    removed backend

--- backend/server.js
+++
@@ -1,8 +0,0 @@
-import { app } from "./app.js";
-
-const PORT = process.env.PORT ?? 7000;
-const HOST = process.env.HOST ?? "0.0.0.0.";
-
-app.listen(PORT,HOST, () => {
-  console.log(`Server listening on port ${PORT}`);
-});
