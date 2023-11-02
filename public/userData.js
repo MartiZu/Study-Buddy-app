f3c16573fceb0cfadb96016666a82936c036c884
MartiZu <martina.zurli@gmail.com>
2023-11-02 09:24:12 +0000

    new changes

--- public/userData.js
+++ public/userData.js
@@ -15,22 +15,82 @@ document.getElementById("userImage").innerHTML = `#profile-pic{
 function logOut() {
   localStorage.clear();
 }
-
-let slider = document.getElementById("starSlider");
+//assigning stars value to rating of frontend
+let sliderFrontend = document.getElementById("fe-starSlider");
 //get hold of the DIV element
-let starsContainer = document.getElementById("stars");
-console.log(starsContainer);
+let starsContainerFrontend = document.getElementById("fe-stars");
+console.log(starsContainerFrontend);
 //target class ""star-icon"
-let icon = document.querySelector(".star-icon");
+let icon1 = document.querySelector(".star-icon1");
 // add starts / or append children
 
-function adjustStars() {
+function adjustStarsFrontend() {
   //remove stars
-  starsContainer.innerHTML = null;
-  for (let i = 1; i <= slider.value; i++) {
-    const star = icon.cloneNode(true);
-    starsContainer.appendChild(star);
+  starsContainerFrontend.innerHTML = null;
+  for (let i = 1; i <= sliderFrontend.value; i++) {
+    const star = icon1.cloneNode(true);
+    starsContainerFrontend.appendChild(star);
   }
+  // Store the frontend slider value in localStorage
+  localStorage.setItem("feSliderValue", sliderFrontend.value);
+}
+sliderFrontend.addEventListener("input", adjustStarsFrontend);
+// Retrieve the frontend slider value from localStorage and set it
+const savedFeSliderValue = localStorage.getItem("feSliderValue");
+if (savedFeSliderValue !== null) {
+  sliderFrontend.value = savedFeSliderValue;
+  adjustStarsFrontend(); // Update stars based on the stored value
 }
 
-slider.addEventListener("input", adjustStars);
+//assigning stars value to rating of backend
+let sliderBackend = document.getElementById("be-starSlider");
+//get hold of the DIV element
+let starsContainerBackend = document.getElementById("be-stars");
+console.log(starsContainerBackend);
+//target class ""star-icon"
+let icon2 = document.querySelector(".star-icon2");
+// add starts / or append children
+
+function adjustStarsBackend() {
+  //remove stars
+  starsContainerBackend.innerHTML = null;
+  for (let i = 1; i <= sliderBackend.value; i++) {
+    const star = icon2.cloneNode(true);
+    starsContainerBackend.appendChild(star);
+  }
+  // Store the backend slider value in localStorage
+  localStorage.setItem("beSliderValue", sliderBackend.value);
+}
+sliderBackend.addEventListener("input", adjustStarsBackend);
+const savedBeSliderValue = localStorage.getItem("beSliderValue");
+if (savedBeSliderValue !== null) {
+  sliderBackend.value = savedBeSliderValue;
+  adjustStarsBackend(); // Update stars based on the stored value
+}
+
+//assigning stars value to rating of ui/ux
+let sliderUI = document.getElementById("ui-starSlider");
+//get hold of the DIV element
+let starsContainerUI = document.getElementById("ui-stars");
+console.log(starsContainerUI);
+//target class ""star-icon"
+let icon3 = document.querySelector(".star-icon3");
+// add starts / or append children
+
+function adjustStarsUI() {
+  //remove stars
+  starsContainerUI.innerHTML = null;
+  for (let i = 1; i <= sliderUI.value; i++) {
+    const star = icon3.cloneNode(true);
+    starsContainerUI.appendChild(star);
+  }
+  localStorage.setItem("uiSliderValue", sliderUI.value);
+}
+
+sliderUI.addEventListener("input", adjustStarsUI);
+
+const savedSliderValue = localStorage.getItem("uiSliderValue");
+if (savedSliderValue !== null) {
+  sliderUI.value = savedSliderValue;
+  adjustStarsUI(); // Update stars based on the stored value
+}
